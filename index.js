2b5ab83f9c2a6dc57997ff40265c05ec889cb98d
Khiem Thanh Phung <khiemphung@Khiems-MBP.attlocal.net>
2022-10-05 18:48:32 -0700

    add team

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const inquirer = require("inquirer");
 
 
+function createTeam(){
+    inquirer.prompt([{
+        type: "list",
+        message: "What type of employee would you be add to your team?",
+        name: "addTeamRole",
+        choices: ["Manager", "Engineer", "Intern"]
+    }]).then(function (userInput){
+        switch(userInput.addTeamRole) {
+            case "Manager":
+                addManager();
+                break;
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                break;
+            
+            default:
+                htmlBuilder();
+        }
+    })
+    
+}
