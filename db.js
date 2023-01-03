cd953c4d4725cfdd8b8d950e3f909baba3b06b69
Khiem Thanh Phung <khiemphung@khiems-mbp.attlocal.net>
2023-01-02 18:45:57 -0800

    finsh index.js

--- db.js
+++ db.js
@@ -9,3 +9,23 @@ class DB {
    
     viewDepartments = () => {
         return this.connection.promise().query("select * from department")
+    }
+   
+    addDepartment = (name) => {
+        return this.connection.promise().query("insert into department (name) values (?) ", name)
+
+    }
+
+    addRole = (name,salary,department) => {
+        return this.connection.promise().query("insert into role (title, salary, department_id) values (?, ?, ?) ", [name, salary, department])
+
+    }
+    addEmployee = (firstname, lastname,role, manager ) => {
+        return this.connection.promise().query("insert into employee (first_name, last_name, role_id, manager_id) values (?, ?, ?, ?) ", [firstname,lastname,role,manager])
+
+    }
+}
+
+
+
+module.exports = new DB(db)
--- index.js
+++ index.js
@@ -1,26 +1,149 @@
 const inquirer = require("inquirer");
+const db = require("./db")
+const mainQuestions = [
+    {
+        type: 'list',
+        name: 'option',
+        message: 'What do you want to do?',
+        choices: [
+            { name: "view all employess", value: "VIEW_EMPLOYEES" },
+            { name: "view all departments", value: "VIEW_DEPARTMENTS" },
+            { name: "view all roles", value: "VIEW_ROLES" },
+            { name: "add a department", value: "ADD_DEPARTMENT" },
+            { name: "add a role", value: "ADD_ROLE" },
+            { name: "add an employee ", value: "ADD_EMPLOYEE" },
+        ]
+    }]
+function main() {
+    inquirer.prompt(mainQuestions).then(answers => {
 
+        switch (answers.option) {
 
-function createTeam(){
-    inquirer.prompt([{
-        type: "list",
-        message: "What type of employee would you be add to your team?",
-        name: "addTeamRole",
-        choices: ["Manager", "Engineer", "Intern"]
-    }]).then(function (userInput){
-        switch(userInput.addTeamRole) {
-            case "Manager":
-                addManager();
-                break;
-            case "Engineer":
-                addEngineer();
-                break;
-            case "Intern":
-                break;
-            
-            default:
-                htmlBuilder();
+            case "VIEW_EMPLOYEES":
+                viewEmployees()
+                break
+            case "VIEW_DEPARTMENTS":
+                viewDepartments()
+                break
+            case "VIEW_ROLES":
+                viewRoles()
+                break
+            case "ADD_DEPARTMENT":
+                addDepartment()
+                break
+            case "ADD_ROLE":
+                addRole()
+                break
+            case "ADD_EMPLOYEE":
+                addEmployee()
         }
+
     })
+}
+main()
+
+const viewEmployees = () => {
+
+    db.viewEmployees().then(([rows]) => {
+
+        console.table(rows)
+
+    }).then(() => { main() })
+
+}
+const viewDepartments = () => {
+
+    db.viewDepartments().then(([rows]) => {
+
+        console.table(rows)
+    }).then(() => { main() })
+
+}
+const viewRoles = () => {
+
+    db.viewRoles().then(([rows]) => {
+
+        console.table(rows)
+    }).then(() => { main() })
+}
+
+const addDepartment = () => {
+    inquirer.prompt([{
+
+        name: "name",
+        type: "input",
+        message: "What is the name of the department would you like to add?",
+
+    }]).then(answer => {
+
+        db.addDepartment(answer.name).then(([rows]) => {
+
+            console.table(rows)
+
+
+        }).then(() => { main() })
+    }
+    )
+}
+const addRole = () => {
+
+    inquirer.prompt([{
+        name: "name",
+        type: "input",
+        message: "What is the name of the title of the role would you like to add?",
+    },
+    {
+        name: "salary",
+        type: "input",
+        message: "What is the salary of the role?",
+},
+{
+    name: "department",
+    type: "input",
+    message: "What is the name of the department?",
+}
+]).then(answer => {
+
+        db.addRole(answer.name, answer.salary, answer.department).then(([rows]) => {
+
+            console.table(rows)
+
+
+        }).then(() => { main() })
+    }
+    )
+}
+const addEmployee = () => {
+    inquirer.prompt([{
     
+        name: "firstname",
+        type: "input",
+        message: "What is the first name of the Employee would you like to add?",
+    },
+    {
+        name: "lastname",
+        type: "input",
+        message: "What is the last name of the Employee would you like to add?",
+
+    },
+    {
+        name: "role",
+        type: "input",
+        message: "What is the role of the Employee would you like to add?",
+    },
+    {
+        name: "manager",
+        type: "input",
+        message: "What is the first name of the manager would you like to add?",
+    }
+]).then(answer => {
+
+        db.addEmployee(answer.firstname, answer.lastname,answer.role,answer.manager).then(([rows]) => {
+
+            console.table(rows)
+
+
+        }).then(() => { main() })
+    }
+    )
 }
